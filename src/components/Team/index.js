5379b0d5501cb171ebf40de1c2cef7be546c2f2b
Fisnik Mela <fisnikmela@gmail.com>
2020-01-23 11:52:50 +0100

    list employees with contentful data

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle } from 'reactstrap';
 import "./styles.css";
 import * as contentful from 'contentful';
 
@@ -9,16 +10,51 @@ var client = contentful.createClient({
 })
 
 class Team extends Component {
+    state = {
+        employees: []
+    }
+
+    componentDidMount() {
+        this.getContent()
+    }
+
+    getContent = () => {
+        const employee_list = []
+        client.getEntries().then(entries => {
+            entries.items.forEach(entry => {
+                employee_list.push(entry.fields)
+                this.setState({employees: employee_list})
+            })
+        })
+        .catch((error) => {
+            console.log("Error occurred while fetching Entries")
+            console.error(error)
+        })
+    }
+    
     render() {
-        console.log("CLIENT", client);
+        const { employees } = this.state;
         return (
             <div className="team-section">
                 <Container className="team-ctn">
                     <Row>
-                        <Col className="team-details">
-                            <h1>Welcome to Tarmac Website</h1>            
-                        </Col>
+                        { employees.map((employee) => {
+                            return (
+                                <Col xs="4" className="team-details" >
+                                    <Card>
+                                        <CardImg top width="100%" src={employee.photo.fields.file.url} alt="Card image cap" />
+                                        <CardBody>
+                                            <CardTitle>{employee.fullName}</CardTitle>
+                                            <CardSubtitle>{employee.position}</CardSubtitle>
+                                            <CardText>{employee.location}</CardText>
+                                        {/* <Button>Button</Button> */}
+                                        </CardBody>
+                                    </Card>           
+                                </Col>
+                            )
+                        })}
                     </Row>
+            
                 </Container>
             </div>
     )}
